8d55a74fa17bea80afa97b021f76b9fba2927ec2
alvgarfer <alvgarfer@gmail.com>
2017-01-16 15:46:00 +0100

    Act letra dni

--- EjemplosBasicosAlvaro/src/javaa/strings/LetraDni.java
+++ EjemplosBasicosAlvaro/src/javaa/strings/LetraDni.java
@@ -7,23 +7,27 @@ public class LetraDni
 	
 	public static String caracteresDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
 	
+	
+	public static Scanner Pedir_numero_dni ()
+	{
+		Scanner leer_dni = new Scanner (System.in);
+		
+		return leer_dni;
+	}
+	
 	public static int letra_dni ()
 	{
-		char letra_dni = 0;
+		int dni = Integer.parseInt (Pedir_numero_dni ().next() );
+		int calculo_letra = dni % 23;
 
-		Scanner leer_dni = new Scanner (System.in);
-
-		int dni = Integer.parseInt (leer_dni.next() );
-		int calculo = dni % 23;
-
-		return calculo;
+		return calculo_letra;
 	}
 
 	public static void main (String[] args)
 	{
 		System.out.println ("Introduzca su número de DNI.");
 
-		System.out.println ("Su letra de DNI es: " + caracteresDNI.charAt (letra_dni() )); 
+		System.out.println ("Su letra de DNI es: " + caracteresDNI.charAt (letra_dni() ) ); 
 	}
 }
 
