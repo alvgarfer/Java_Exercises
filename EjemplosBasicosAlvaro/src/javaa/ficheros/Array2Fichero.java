f626c1950e00d3d162334a722f9a022b60f2f4a7
alvgarfer <alvgarfer@gmail.com>
2017-01-19 18:52:50 +0100

    Manejo de Ficheros

---
+++ EjemplosBasicosAlvaro/src/javaa/ficheros/Array2Fichero.java
@@ -0,0 +1,32 @@
+package javaa.ficheros;
+
+import java.io.File;
+
+public class Array2Fichero
+{
+	
+	public static String [] fromFichero2Array (File file)
+	{
+		String [] lista_cadena = null;
+		
+		
+		
+		return lista_cadena;
+	}
+
+	public static boolean fromArray2Fichero (String[] lista_cadena)
+	{
+		boolean ok = false;
+		
+		return ok;
+	
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
