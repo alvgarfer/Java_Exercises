00ff499a6a011e0db067703e320edee110a622c0
alvgarfer <alvgarfer@gmail.com>
2017-01-26 18:30:08 +0100

    programa  base datos y liberar recursos

--- EjemplosBasicosAlvaro/src/javaa/basedatosjava/LiberarRecursos.java
+++ EjemplosBasicosAlvaro/src/javaa/basedatosjava/LiberarRecursos.java
@@ -1,20 +1,45 @@
 package javaa.basedatosjava;
 
-public class LiberarRecursos 
+// Programa de ejemplo de como operar con una base de datos y liberación de recursos
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LiberarRecursos
 {
-	try
+	public static void liberarRecursos (ResultSet rset, Statement stat)
 	{
-		// las acciones que quieres realizar
-	}
-	catch(Exception e) //captura las excepciones
-	{
-		e.printStackTrace();
+		if (rset != null)	{ try { rset.close(); } catch (Exception e) { e.printStackTrace(); }}
+		if (stat != null)	{ try {	stat.close(); } catch (Exception e) { e.printStackTrace(); }}
 	}
 	
-	finally // libero recursos, de "adentro a fuera" , ResultSet, Statment, Conexion
-	{		// cierras la conexion con la base de datos
-		if (rset != null) 	{ try { rset.close(); } catch (Exception e2) { e2.printStackTrace(); }}
-		if (stmt != null)	{ try {	stmt.close(); } catch (Exception e2) { e2.printStackTrace(); }}
-		if (conn != null) 	{ try { conn.close(); } catch (Exception e4) { e4.printStackTrace(); }}
-	}   
+	public static void liberarConexion (Connection conn)
+	{
+		if (conn != null) { try { conn.close(); } catch (Exception e) { e.printStackTrace(); }}	
+	}
+
+	public static void main(String[] args) throws SQLException
+	{
+		ResultSet rset = null;
+		Statement stat = null;
+		Connection conn = null;
+		
+		try
+		{
+			// las acciones que quieres realizar
+		}
+		catch(Exception e) //captura las excepciones
+		{
+			e.printStackTrace();
+		}
+		
+		finally // libero recursos, de "adentro a fuera" , ResultSet, Statment, Conexion
+		{		// cierras la conexion con la base de datos
+			liberarRecursos(rset, stat);
+			liberarConexion(conn);
+		} 
+	}
 }
+
